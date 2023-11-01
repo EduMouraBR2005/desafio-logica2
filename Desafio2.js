f617f12d082887fcf1a78a96b023f00809afd30d
EduMouraBR2005 <147084035+EduMouraBR2005@users.noreply.github.com>
2023-11-01 19:38:18 -0300

    Add files via upload

---
+++ Desafio2.js
@@ -0,0 +1,28 @@
+function calcularNivel(vitorias, derrotas) {
+    const saldoVitorias = vitorias - derrotas;
+    let nivel = "";
+
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias >= 11 && vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias >= 21 && vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias >= 51 && vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias >= 81 && vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias >= 91 && vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    return `O Herói tem um saldo de ${saldoVitorias} está no nível de ${nivel}`;
+}
+
+// Exemplo de uso:
+const vitorias = 75;
+const derrotas = 20;
+const resultado = calcularNivel(vitorias, derrotas);
+console.log(resultado) = "O Herói tem um saldo de 55 está no nível de Ouro"
